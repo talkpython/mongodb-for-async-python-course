584ee6f9740de03ec493790b06f1e576ca8ed639
Michael Kennedy <michael@talkpython.fm>
2023-06-07 14:15:13 -0700

    Starter code for weather client chapter 5

---
+++ code/ch05-foundations-async/weather_client.py
@@ -0,0 +1,12 @@
+locations = [
+    'https://weather.talkpython.fm/api/weather?city=New York&state=NY&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=Los Angeles&state=CA&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=Chicago&state=IL&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=Houston&state=TX&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=Phoenix&state=AZ&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=Philadelphia&state=PA&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=San Antonio&state=TX&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=San Diego&state=CA&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=Dallas&state=TX&country=US&units=imperial',
+    'https://weather.talkpython.fm/api/weather?city=San Jose&state=CA&country=US&units=imperial',
+]
