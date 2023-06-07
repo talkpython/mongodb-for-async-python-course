a5502b316b365d59785a314be41f75144ee33b24
Michael Kennedy <michael@talkpython.fm>
2023-06-07 14:39:59 -0700

    Weather client with httpx and async-await.

--- code/ch05-foundations-async/weather_client.py
+++ code/ch05-foundations-async/weather_client.py
@@ -1,3 +1,8 @@
+import asyncio
+import datetime
+
+import httpx
+
 locations = [
     'https://weather.talkpython.fm/api/weather?city=New York&state=NY&country=US&units=imperial',
     'https://weather.talkpython.fm/api/weather?city=Los Angeles&state=CA&country=US&units=imperial',
@@ -10,3 +15,45 @@ locations = [
     'https://weather.talkpython.fm/api/weather?city=Dallas&state=TX&country=US&units=imperial',
     'https://weather.talkpython.fm/api/weather?city=San Jose&state=CA&country=US&units=imperial',
 ]
+
+
+async def get_report(url) -> dict:
+    print(f"Calling {url}")
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        resp.raise_for_status()
+
+    return resp.json()
+
+
+async def main_naive():
+    t0 = datetime.datetime.now()
+    for url in locations:
+        report = await get_report(url)
+        show_report(report)
+
+    dt = datetime.datetime.now() - t0
+    print(f"Done in {dt.total_seconds() * 100:,.1f} ms.")
+
+
+async def main():
+    t0 = datetime.datetime.now()
+    tasks = []
+    for url in locations:
+        tasks.append(asyncio.create_task(get_report(url)))
+
+    for task in tasks:
+        report = await task
+        show_report(report)
+
+    dt = datetime.datetime.now() - t0
+    print(f"Done in {dt.total_seconds() * 100:,.1f} ms.")
+
+
+def show_report(report):
+    print(report['forecast'], report['location'])
+
+
+if __name__ == '__main__':
+    asyncio.run(main_naive())
+    asyncio.run(main())
