2747f9d39fa89caa5baa0eda88db26ebd7e3c396
Michael Kennedy <michael@talkpython.fm>
2023-06-25 15:23:16 -0700

    FastAPI server ready.

--- code/ch11-deployment/pypi-app/main.py
+++ code/ch11-deployment/pypi-app/main.py
@@ -29,7 +29,16 @@ def configure_routing():
 
 @api.on_event('startup')
 async def configure_db():
-    await mongo_setup.init_connection('pypi')
+    dev_mode = False
+
+    if dev_mode:
+        await mongo_setup.init_connection('pypi')
+    else:
+        await mongo_setup.init_connection(
+            'pypi', server='10.124.0.4', port=5621,
+            username='pypi-database-user', password='9827a696-6cb9-47cc-a71e-e8b0ef38e2a3',
+            use_ssl=True)
+    # await mongo_setup.init_connection('pypi')
 
 
 @api.get('/', include_in_schema=False)
