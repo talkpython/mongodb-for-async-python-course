43d07c959d493e9f4a4067c45f3fba2f661d6ff8
Michael Kennedy <michael@talkpython.fm>
2023-06-25 15:19:09 -0700

    Locust test, max users style.

--- code/ch12-load-testing/locustfile.py
+++ code/ch12-load-testing/locustfile.py
@@ -3,15 +3,16 @@ import locust
 
 class ApiTest(locust.HttpUser):
     host = "http://127.0.0.1:8000"
+    wait_time = locust.between(5, 30)
 
     @locust.task(weight=1)
     def home_page(self):
         self.client.get('/')
 
-    @locust.task(weight=1)
+    @locust.task(weight=5)
     def stats(self):
         self.client.get('/api/stats')
 
-    @locust.task(weight=1)
+    @locust.task(weight=15)
     def recent(self):
         self.client.get('/api/packages/recent/5')
